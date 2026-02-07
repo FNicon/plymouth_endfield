d54d7cd356ec8f18028778405cd3721e202820f3
fnicon <mybeddrom@gmail.com>
2026-02-07 17:55:52 +0900

    test add package and shas

---
+++ shas.nix
@@ -0,0 +1,6 @@
+{
+  "abstract_ring" = {
+    url = "https://github.com/FNicon/plymouth_endfield/releases/download/v1.0/abstract_ring.tar.gz";
+    sha = "0hfrzm974nm8f237vzlyjjh4pikva3n7fqr1p7jmx04g857g4rjq";
+  };
+}
